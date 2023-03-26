24461f276725329ab619e1f774318e8ca2a717e4
Sanchita-Sharma519 <sanchitasharma519@gmail.com>
2023-03-26 22:19:42 +0530

    1st

---
+++ JOB-PORTAL/backend/app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+//const ejs=require("ejs");
+const morgan=require("morgan");
+const mongoose = require("mongoose");
+//const session=require("express-session");
+//const passport=require("passport");
+//onst passportLocalMongoose=require("passport-local-mongoose");
+var cors = require('cors');
+
+//port
+const port = process.env.PORT || 9000
+
+app.listen(port,function(){
+    console.log("Server started on port 9000");
+});
